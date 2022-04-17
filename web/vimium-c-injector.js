c98b40db078b1fa116ba74ae3ae094735f170d1d
gdh1995 <gdh1995@qq.com>
2022-04-18 02:40:32 +0800

    update scroller and auto-retry on Edge

--- web/vimium-c-injector.js
+++ web/vimium-c-injector.js
@@ -4,34 +4,50 @@
     if (code !== 2) {
       if (code === 3) {
         window.removeEventListener("vimiumMark", onMark, true);
-        vsc = api = vsc2 = oldScroll = null;
+        api = oldScroll = null;
       }
       return;
     }
-    var vsc = window.VSc, api = window.VApi, vsc2 = vsc || api, prop = vsc ? "$sc" : "$"
-    var oldScroll = vsc2[prop];
-    if (vsc2 && typeof vsc2[prop] === "function") {
-      vsc2[prop] = function(element, di, amount) {
-        if (element.id === "viewerContainer" && element.classList.contains("pdfPresentationMode")) {
+    // @ts-ignore
+    var api = window.VApi
+    var oldScroll = api.$;
+    if (api && typeof api.$ === "function") {
+      api.$ = function(element, di, amount) {
+        if (Math.abs(amount) < 0.1) { return oldScroll.apply(this, arguments); }
+        const Presentation = ".pdfPresentationMode"
+        const fullscreenElement = document.fullscreenElement
+        const topEl = fullscreenElement || document.documentElement
+        if ((element === topEl || fullscreenElement && !topEl.contains(element))) {
+          element = fullscreenElement && topEl.matches(Presentation) ? topEl : topEl.querySelector(Presentation);
+        } else {
+          element = element.closest(Presentation);
+        }
+        if (element) {
+          const oldTop = element.scrollTop, oldPage = PDFViewerApplication.page;
           element.dispatchEvent(new WheelEvent("wheel", {
             bubbles: !0, cancelable: !0, composed: !0,
             deltaY: amount
-        }));
+          }));
+          return oldPage !== PDFViewerApplication.page || element.scrollTop !== oldTop;
         } else {
-          oldScroll.call(this, element, di, amount);
+          return oldScroll.apply(this, arguments);
         }
       };
     }
     if (api && (api.u + "").lastIndexOf(".href") > 0) {
       api.u = function () {
+        if (location.pathname.startsWith("/content/web/viewer.html")) {
+          var file = new URLSearchParams(location.search).get("file");
+          if (file) { return file }
+        }
         return location.href.slice(location.origin.length + 1).split("#", 1)[0];
       };
     }
   };
   var onMark = function (event) {
     var a = event.relatedTarget, str = a && a.textContent, box = a && document.getElementById("viewerContainer");
+    event.stopImmediatePropagation();
     if (!box) { return; }
-    box.classList.contains("pdfPresentationMode")
     if (!str) { // generate a mark
       a.textContent = [box.scrollLeft, box.scrollTop];
       return;
@@ -46,13 +62,16 @@
     }
   }
 
+  var IDOnEdge = "aibcglbfblnogfjhbcmmpobjhnomhcdo";
+  var IDOnChrome = "hfjbmagddngcpeloejdejnfgbamkjaeg";
+  var StorageKey = "targetExtensionInjector";
   var doInject = function (injectorURL) {
   if (injectorURL === "nul" || injectorURL === "/dev/null") { return; }
+  var IsEdge = false, hasKnown = !!injectorURL;
   if (!injectorURL) {
-    injectorURL = /\sEdg\//.test(navigator.appVersion) ? "aibcglbfblnogfjhbcmmpobjhnomhcdo"
-        : "hfjbmagddngcpeloejdejnfgbamkjaeg";
+    IsEdge = /\sEdg\//.test(navigator.userAgent);
+    injectorURL = IsEdge ? IDOnEdge : IDOnChrome;
     injectorURL = "chrome-extension://" + injectorURL + "/lib/injector.js";
-    localStorage.setItem("targetExtensionInjector", injectorURL);
   }
   var script = document.createElement("script");
   script.src = injectorURL;
@@ -60,14 +79,28 @@
   script.onload = function () {
     var injector = window.VimiumInjector;
     if (injector && location.pathname.indexOf("://") >= 0) {
+      if (!hasKnown) {
+        localStorage.setItem(StorageKey, this.src);
+      }
       injector.cache ? VimiumCHandler(2, "") : injector.callback = VimiumCHandler;
       window.addEventListener("vimiumMark", onMark, true)
     }
     return !!injector
   };
+  if (IsEdge) {
+    script.onerror = function () {
+      this.remove();
+      var script2 = document.createElement("script");
+      script2.src = "chrome-extension://" + IDOnChrome + "/lib/injector.js";
+      script2.async = true;
+      script2.defer = false;
+      script2.onload = this.onload;
+      document.head.appendChild(script2);
+    }
+  }
   document.head.appendChild(script);
   };
-  var injectorPromise = localStorage.getItem("targetExtensionInjector");
+  var injectorPromise = localStorage.getItem(StorageKey);
   if (injectorPromise instanceof Promise) {
     injectorPromise.then(doInject);
   } else {
